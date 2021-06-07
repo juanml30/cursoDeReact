b07a1d7008aa50a905f8820aecf4aa1bec56427e
juanml30 <moreno.l.juan@gmail.com>
2021-06-07 04:42:13 -0300

    me creo un archivo llamado global css

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,25 +1,32 @@
 import React from "react";
 
+import "./styles/Badge.css";
 import confLogo from "../images/badge-header.svg";
 
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="Badge">
+        <div className="Badge__header">
           <img src={confLogo} alt="Logo de la conferencia" />
         </div>
-        <div>
-          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+
+        <div className="Badge__section-name">
+          <img
+            src="https://www.gravatar.com/avatar?d=identicon"
+            alt="Avatar"
+            className="Badge__avatar"
+          />
           <h1>
             Juan
             <br />
             Moreno
           </h1>
         </div>
-        <div>
+        <div className="Badge__section-info">
           <p>Frontend Developer</p>
         </div>
+        <div className="Badge__footer">#platzi</div>
       </div>
     );
   }
