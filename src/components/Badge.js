336ef2b4cc86c49784fd8e54e6e21da0c557934c
juanml30 <moreno.l.juan@gmail.com>
2021-06-07 04:24:58 -0300

    agrego una carpeta style y le agrego un archivo css para dar algo de estilo

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,14 +1,16 @@
 import React from "react";
 
+import confLogo from "../images/badge-header.svg";
+
 class Badge extends React.Component {
   render() {
     return (
       <div>
         <div>
-          <img src="" alt="Logo de la conferencia" />
+          <img src={confLogo} alt="Logo de la conferencia" />
         </div>
         <div>
-          <img src="{confLogo}" alt="Avatar" />
+          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
           <h1>
             Juan
             <br />
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 
 import Badge from "./components/Badge";
 
-const element = <h1>Hello, Platzi Hecho por Juan!</h1>;
-
 const container = document.getElementById("app");
 
 // ReactDOM.render(__qué__, __dónde__);
